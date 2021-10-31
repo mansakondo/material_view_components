50bb3ba16cfedc0793c6c5bb918e87f10abf5f76
mansakondo <mansakondo22@gmail.com>
2021-10-31 16:39:44 +0100

    Add `esbuild-plugin-import-glob` plugin

--- build.js
+++ build.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+const importGlobPlugin = require("esbuild-plugin-import-glob");
+
 require("esbuild").build({
   entryPoints: ["app/javascript/application.js"],
   outdir: "app/assets/builds/material_view_components",
   bundle: true,
+  plugins: [
+    importGlobPlugin.default(),
+  ],
   watch: {
     onRebuild: (err, result) => {
       if (err) console.log("Watch build failed: ", err)
