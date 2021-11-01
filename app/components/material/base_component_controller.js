edcf7afc7a9f560484d50e8aacbcd53a2ff939fe
mansakondo <mansakondo22@gmail.com>
2021-11-01 20:32:05 +0100

    Encapsulate styles using server-side shadow roots

--- app/components/material/base_component_controller.js
+++ app/components/material/base_component_controller.js
@@ -1,13 +1,28 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  connect(customElementCallback) {
+  connect() {
+    if (this.supportsDeclarativeShadowDOM()) {
+      console.log(`${this.identifier} controller is connected`);
+      return this
+    }
+
     const elementName = this.identifier.replaceAll("--", "-")
 
     if (!customElements.get(elementName)) {
       customElements.define(elementName, class extends HTMLElement {
-        connectedCallback() { customElementCallback.bind(this)() }
+        connectedCallback() {
+          const shadow    = this.attachShadow({mode: "open"})
+          const template  = this.querySelector("template")
+
+          shadow.appendChild(template.content)
+        }
       })
     }
+
+  }
+
+  supportsDeclarativeShadowDOM() {
+    return HTMLTemplateElement.prototype.hasOwnProperty('shadowRoot')
   }
 }
--- app/components/material/button_component_controller.js
+++ app/components/material/button_component_controller.js
@@ -2,13 +2,6 @@ import BaseComponentController from "./base_component_controller";
 
 export default class extends BaseComponentController {
   connect() {
-    super.connect(function() {
-      const shadow = this.attachShadow({mode: "open"})
-      const style  = this.querySelector("link")
-      const button = this.querySelector("button")
-
-      shadow.appendChild(style)
-      shadow.appendChild(button)
-    })
+    super.connect()
   }
 }
