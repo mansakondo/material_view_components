9811754fae9474498567f59d7d89ec113555724c
mansakondo <mansakondo22@gmail.com>
2021-11-01 21:04:55 +0100

    Generate `Material::BaseComponent` component

---
+++ test/components/material/base_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class Material::BaseComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Material::BaseComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
