f4db02af9b5c42e7a443bb3c62b20f8469599de3
mansakondo <mansakondo22@gmail.com>
2021-11-04 15:45:28 +0100

    Move CSS classes to `#classes`

--- app/components/material/button_component.rb
+++ app/components/material/button_component.rb
@@ -6,4 +6,16 @@ class Material::ButtonComponent < ViewComponent::Base
   def initialize(text:)
     @text = text
   end
+
+  def classes
+    string = <<-CLASSES
+    material-bg-primary material-px-6 material-h-10 material-rounded-full
+    material-text-on-primary material-text-label-large material-font-label
+    material-leading-label-large
+    CLASSES
+
+    string
+      .strip
+      .gsub(/\s{2,}|\n/, " ")
+  end
 end
