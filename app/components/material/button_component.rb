79faa9a3cd6e17282a6e9a8220ea01e6bba64dbf
mansakondo <mansakondo22@gmail.com>
2021-11-05 17:10:13 +0100

    Refactor `#classes`

--- app/components/material/button_component.rb
+++ app/components/material/button_component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Material::ButtonComponent < ViewComponent::Base
+class Material::ButtonComponent < ApplicationComponent
   renders_one :text, -> (text) do
     content_tag :span do
       text
@@ -20,15 +20,11 @@ class Material::ButtonComponent < ViewComponent::Base
   end
 
   def classes
-    string = <<-CLASSES
+    super <<-CLASSES
     material-flex material-items-center #{icon ? "material-pl-4 material-pr-6" :
     "material-px-6"} material-h-10 material-bg-primary material-rounded-full
     material-text-on-primary material-text-label-large material-font-medium
     material-font-label material-leading-label-large
     CLASSES
-
-    string
-      .strip
-      .gsub(/\s{2,}|\n/, " ")
   end
 end
