6f929f70fb9fcd62b575671dfd6e66ff2e25d6f8
mansakondo <mansakondo22@gmail.com>
2021-11-05 15:47:37 +0100

    Convert `:text` attribute to slot

--- app/components/material/button_component.rb
+++ app/components/material/button_component.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Material::ButtonComponent < ViewComponent::Base
-  attr_reader :text
-
-  def initialize(text:)
-    @text = text
+  renders_one :text, -> (text) do
+    content_tag :span do
+      text
+    end
   end
 
   def classes
