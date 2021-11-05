ab22e1fd4003a052e7da8a9160f6c26a20597539
mansakondo <mansakondo22@gmail.com>
2021-11-05 15:56:23 +0100

    Add `:icon` slot

--- app/components/material/button_component.rb
+++ app/components/material/button_component.rb
@@ -7,9 +7,16 @@ class Material::ButtonComponent < ViewComponent::Base
     end
   end
 
+  renders_one :icon, -> (name) do
+    content_tag :span, class: "material-icons material-pr-2" do
+      name
+    end
+  end
+
   def classes
     string = <<-CLASSES
-    material-bg-primary material-px-6 material-h-10 material-rounded-full
+    material-flex material-items-center #{icon ? "material-pl-4 material-pr-6" :
+    "material-px-6"} material-h-10 material-bg-primary material-rounded-full
     material-text-on-primary material-text-label-large material-font-medium
     material-font-label material-leading-label-large
     CLASSES
