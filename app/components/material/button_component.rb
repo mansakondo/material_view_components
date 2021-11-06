0d65c720a133a00d755d9c5611c33f70eccc8353
mansakondo <mansakondo22@gmail.com>
2021-11-06 21:35:38 +0100

    Add `disabled` button option

--- app/components/material/button_component.rb
+++ app/components/material/button_component.rb
@@ -19,17 +19,45 @@ class Material::ButtonComponent < ApplicationComponent
     end
   end
 
-  def initialize(name = "", html_options = {})
+  attr_reader :disabled
+
+  def initialize(name = "", html_options = {}, disabled: false)
     @name         = name
-    @html_options = html_options
+    @html_options = disabled ? html_options.merge(disabled: "disabled") : html_options
+    @disabled     = disabled
   end
 
   def classes
-    super <<-CLASSES
+    sanitize <<-CLASSES
     material-relative #{icon ? "material-pl-4 material-pr-6" : "material-px-6"}
-    material-h-10 material-bg-primary material-rounded-full
-    material-text-on-primary material-text-label-large material-font-medium
-    material-font-label material-leading-label-large
+    material-h-10  material-rounded-full
+    #{disabled ? "material-text-on-surface material-cursor-default" :
+    "material-bg-primary material-text-on-primary"} material-text-label-large
+    material-font-medium material-font-label material-leading-label-large
+    CLASSES
+  end
+
+  def content_classes
+    sanitize <<-CLASSES
+    material-flex material-items-center #{"material-opacity-disabled-content" if
+    disabled}
+    CLASSES
+  end
+
+  def overlay_classes
+    state_classes =
+      if disabled
+        "material-bg-on-surface material-opacity-disabled-container"
+      else
+        <<-CLASSES
+        material-bg-on-primary material-opacity-0 hover:material-opacity-hover
+        focus:material-opacity-focus active:material-opacity-pressed
+        CLASSES
+      end
+
+    sanitize <<-CLASSES
+    material-absolute material-w-full material-inset-0 material-rounded-full
+    #{state_classes}
     CLASSES
   end
 end
