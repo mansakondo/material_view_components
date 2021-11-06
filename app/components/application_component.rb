23ba57186e44f5055095a9adc79219f3baea65ef
mansakondo <mansakondo22@gmail.com>
2021-11-06 17:11:55 +0100

    Add `ApplicationComponent#identifier`

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationComponent < ViewComponent::Base
+  def identifier
+    klass = self.class
+
+    klass.name
+      .tableize
+      .singularize
+      .gsub("/", "--")
+      .gsub("_", "-")
+  end
+
   def classes(string)
     string
       .strip
