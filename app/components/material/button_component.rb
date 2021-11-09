9e7d1cbf15151e0f9c3a852cab17d621b43b96c2
mansakondo <mansakondo22@gmail.com>
2021-11-09 19:14:27 +0100

    Add `:tonal` button type

--- app/components/material/button_component.rb
+++ app/components/material/button_component.rb
@@ -77,4 +77,39 @@ class Material::ButtonComponent < ApplicationComponent
     #{state_classes}
     CLASSES
   end
+
+  def tonal_button_container_classes
+    sanitize <<-CLASSES
+    material-relative #{icon ? "material-pl-4 material-pr-6" : "material-px-6"}
+    material-h-10  material-rounded-full #{disabled ? "material-cursor-default"
+    : "material-bg-secondary-container dark:material-bg-secondary-container-on-dark"}
+    CLASSES
+  end
+
+  def tonal_button_content_classes
+    sanitize <<-CLASSES
+    material-flex material-items-center #{disabled ? "material-text-on-surface
+    dark:material-text-on-surface-on-dark material-opacity-disabled-content" :
+    "material-text-on-secondary-container dark:material-text-on-secondary-container-on-dark"}
+    material-text-label-large material-font-medium material-font-label
+    material-leading-label-large material-tracking-label-large
+    CLASSES
+  end
+
+  def tonal_button_overlay_classes
+    state_classes =
+      if disabled
+        "material-bg-on-surface dark:material-bg-on-surface-on-dark material-opacity-disabled-container"
+      else
+        <<-CLASSES
+        material-bg-on-surface dark:material-bg-on-surface-on-dark material-opacity-0 hover:material-opacity-hover
+        focus:material-opacity-focus active:material-opacity-pressed
+        CLASSES
+      end
+
+    sanitize <<-CLASSES
+    material-absolute material-w-full material-inset-0 material-rounded-full
+    #{state_classes}
+    CLASSES
+  end
 end
