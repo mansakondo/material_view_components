be105c515035d2ea0d2acd6b58326be7d838e345
mansakondo <mansakondo22@gmail.com>
2021-11-09 21:04:02 +0100

    Add `:outlined` button type

--- app/components/material/button_component.rb
+++ app/components/material/button_component.rb
@@ -114,4 +114,34 @@ class Material::ButtonComponent < ApplicationComponent
     #{state_classes}
     CLASSES
   end
+
+  def outlined_button_container_classes
+    sanitize <<-CLASSES
+    material-relative #{icon ? "material-pl-4 material-pr-6" : "material-px-6"}
+    material-h-10 dark:material-outline-on-dark material-rounded-full
+    #{disabled ? "material-cursor-default material-outline-disabled
+    dark:material-outline-disabled-on-dark" : "material-bg-surface
+    dark:material-bg-surface-on-dark material-outline-default
+    hover:material-shadow"}
+    CLASSES
+  end
+
+  def outlined_button_content_classes
+    sanitize <<-CLASSES
+    material-flex material-items-center #{disabled ? "material-text-on-surface
+    dark:material-text-on-surface-on-dark material-opacity-disabled-content" :
+    "material-text-primary dark:material-text-primary-on-dark"}
+    material-text-label-large material-font-medium material-font-label
+    material-leading-label-large material-tracking-label-large
+    CLASSES
+  end
+
+  def outlined_button_overlay_classes
+    sanitize <<-CLASSES
+    material-absolute material-w-full material-inset-0 material-rounded-full
+    material-bg-primary dark:material-bg-primary-on-dark material-opacity-0
+    hover:material-opacity-hover focus:material-opacity-focus
+    active:material-opacity-pressed
+    CLASSES
+  end
 end
