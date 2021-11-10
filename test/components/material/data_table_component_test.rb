ddfa8a4b005fc2cd3e335266073dc7203ebe1baa
mansakondo <mansakondo22@gmail.com>
2021-11-10 11:03:11 +0100

    Generate `Material::DataTableComponent`

---
+++ test/components/material/data_table_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class Material::DataTableComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Material::DataTableComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
