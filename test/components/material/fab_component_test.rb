1d1f5a984d95e1a9fc7dddde1a1a59d36047c84b
mansakondo <mansakondo22@gmail.com>
2021-11-11 11:42:30 +0100

    Generate `Material::FABComponent`

---
+++ test/components/material/fab_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class Material::FABComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Material::FABComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
