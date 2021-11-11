699ec4fe20521cb2d42b70324e20ade130c0e3a7
mansakondo <mansakondo22@gmail.com>
2021-11-11 11:37:54 +0100

    Add `Material::DataTableComponent` component

--- app/components/material/data_table_component.rb
+++ app/components/material/data_table_component.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Material::DataTableComponent < ApplicationComponent
+  attr_reader :headers
+  attr_reader :items
 
+  def initialize(headers:, items:)
+    @headers = headers
+    @items   = items
+  end
 end
